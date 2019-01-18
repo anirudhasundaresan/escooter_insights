6060765ae981cdd8a99f005cfe94cc139847ce59
anirudhasundaresan <anirudhasundaresan@outlook.com>
2019-01-18 16:39:58 -0500

    need to think of a way to track birds

--- birds.py
+++ birds.py
@@ -10,22 +10,23 @@ Obtaining API acess:
 - Write the listing to a CSV
 '''
 
-# Birds are available only between 4AM - 9PPM. 
+# Birds are available only between 4AM - 9PPM.
 # I will focus on getting the Bird bike data first.
 import requests
 import json
 from random import randint
+import pprint
 
 url = 'https://api.bird.co/user/login'
 
 # str(randint(0,900))
 
-payload = {"email": "ihfde" + "@vfdv.cm"}
+payload = {"email": "ife" + "@vfdv.cm"}
 headers = {'Device-id': '123e4567-e89b-12d3-a456-426655440000', 'Platform': 'ios', 'Content-type': 'application/json'}
 
 r = requests.post(url, data=json.dumps(payload), headers=headers)
-print(r.text) # this is a string! - not a dictionary
-print(type(r.text))
+# print(r.text) # this is a string! - not a dictionary
+# print(type(r.text))
 
 token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBVVRIIiwidXNlcl9pZCI6ImM5YTVlNzM2LWRlMjctNGFkNy1hMGI2LTA2OWZkMWJhN2YzMyIsImRldmljZV9pZCI6IjEyM2U0NTY3LWU4OWItMTJkMy1hNDU2LTQyNjY1NTQ0MDkwMCIsImV4cCI6MTU3OTMzMjA1NH0.-LA0hg3gr9hPKXKLrvSPbElggWrJjDZDWUpPXJmGWhw"
 
@@ -39,13 +40,35 @@ This token has an expiry time, but this gets extended everytime you call the abo
 This token is what we will be using to get the location and data about the bikes.
 '''
 
-url_ = 'https://api.bird.co/bird/nearby?latitude=37.77216&longitude=-122.409485&radius=000'
+url_ = 'https://api.bird.co/bird/nearby?latitude=33.775620&longitude=-84.396286&radius=00.0000111'
 headers_ = {'Authorization': 'Bird {}'.format(token), 'Device-id': '123e4567-e89b-12d3-a456-426655440000', 'App-Version': '3.0.5', \
-'Location': '{"latitude":37.77216, "longitude":"-122.409485","altitude":500,"accuracy":100,"speed":-1,"heading":-1}'}
+'Location': '{"latitude":33.775620, "longitude":"-84.396286","altitude":500,"accuracy":100,"speed":-1,"heading":-1}'}
 # see how altitude, accuracy, speed and heading matter?
 
 r = requests.get(url_, headers=headers_)
-print(r)
-print(r.text)
+# print(r)
+parsed_data = json.loads(r.text)
 
+'''
+for key in parsed_data:
+    print("Key: ", key)
+
+print("Under key=birds: ", parsed_data['birds'], " and number of birds in this radius: ", len(parsed_data['birds']))
+print("Under key=clusters", parsed_data['clusters'], "and number of clusters: ", len(parsed_data['clusters']))
+print("Under key=areas", parsed_data['areas'], "and number of areas: ", len(parsed_data['areas']))
+'''
+'''
+with open('birds.csv', mode='w') as in_file:
+    print("ID, Latitude, Longitude, Code, Captive, Battery_level \n")
+    for id in parsed_data['birds']: # is a list
+        print(id['id'], ',', id['location']['latitude'], ',', id['location']['longitude'], ',', id['code'], ',', id['captive'], ',', id['battery_level'], '\n')
 # see how you are going to manage data.
+'''
+
+ls = []
+# check if the ids from these bikes are all unique, and that each id represents the same bike.
+for id in parsed_data['birds']:
+    ls.append(id['id'])
+print(len(ls))
+ls.sort()
+print(ls)
